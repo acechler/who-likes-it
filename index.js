ed63b994a21d6b148a3628d3996609d8a5691bd5
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-04 18:47:39 -0500

    Develop a template of conditions

--- index.js
+++ index.js
@@ -6,12 +6,27 @@ function likes(names) {
     // Array has no elements           -> no one likes this
     // Array has one element           -> Peter likes this
     // Array has two elements          -> Peter and Jim like this
-    // Array has three elements        -> Peter, Jim, and Corey like this.
-    // Array has four or greater       -> Peter, Jim and {size-2} like this.
-    const message = `${names[0]}, and ${names[1]} likes this.`;
+    // Array has three elements        -> Peter, Jim, and Corey like this
+    // Array has four or greater       -> Peter, Jim and {length-2} like this
+
+    //const message = `${names[0]}, and ${names[1]} likes this.`;
+    let message;
+    console.log(names.length);
+    if(names.length === 0){
+        message = `No one likes this`;
+    } else if(names.length === 1){
+        message = `${names[0]} likes this`;
+    } else if(names.length === 2){
+        message = `${names[0]} and ${names[1]} like this`;
+    } else if(names.length === 3){
+        message = `${names[0]}, ${names[1]}, and ${names[2]} like this`;
+    } else if(names.length === 4){
+        message = `${names[0]}, ${names[1]} and ${names.length-2} like this`;
+    }
     return message;
 }
 
 
 const localnames = ["bob","jim"];
-console.log(likes(localnames));
+const nonames = [];
+console.log(likes(nonames));
