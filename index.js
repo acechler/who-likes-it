d2cbaa9bbb4cc5e9040d177ed82b5d88ca150ddb
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-04 18:24:32 -0500

    Write down pseudocode for how to deal with the different possible array conditions.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
 
 function likes(names) {
+
+    /// Conditions
+    // Array has no elements           -> no one likes this
+    // Array has one element           -> Peter likes this
+    // Array has two elements          -> Peter and Jim like this
+    // Array has three elements        -> Peter, Jim, and Corey like this.
+    // Array has four or greater       -> Peter, Jim and {size-2} like this.
     const message = `${names[0]}, and ${names[1]} likes this.`;
     return message;
 }
