d24412f7538173a7d9f8103e64b2848935fd8c83
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-04 18:18:06 -0500

    Figuring out how to pass an array into function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
 
 function likes(names) {
-    // TODO
+    const message = `${names[0]}, and ${names[1]} likes this.`;
+    return message;
 }
+
+
+const localnames = ["bob","jim"];
+console.log(likes(localnames));
