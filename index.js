e26fa066581cfc5813dec9433f042babf25e453a
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-04 19:17:38 -0500

    Finish up challenge

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 // https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
 
 function likes(names) {
-
-    /// Conditions
-    // Array has no elements           -> no one likes this
-    // Array has one element           -> Peter likes this
-    // Array has two elements          -> Peter and Jim like this
-    // Array has three elements        -> Peter, Jim, and Corey like this
-    // Array has four or greater       -> Peter, Jim and {length-2} like this
-
-    //const message = `${names[0]}, and ${names[1]} likes this.`;
     let message;
     if(names.length === 0){
-        message = `No one likes this`;
+        message = `no one likes this`;
     } else if(names.length === 1){
         message = `${names[0]} likes this`;
     } else if(names.length === 2){
         message = `${names[0]} and ${names[1]} like this`;
     } else if(names.length === 3){
-        message = `${names[0]}, ${names[1]}, and ${names[2]} like this`;
+        message = `${names[0]}, ${names[1]} and ${names[2]} like this`;
     } else if(names.length >= 4){
         let remainder = names.length - 2;
-        message = `${names[0]}, ${names[1]} and ${remainder} like this`;
+        message = `${names[0]}, ${names[1]} and ${remainder} others like this`;
     } else {
 
     }
